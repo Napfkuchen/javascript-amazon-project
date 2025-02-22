e8ee789348441cdb58b91324fb75ac96c23cbd39
Sergei Fladung <sergeifladung@web.de>
2025-02-23 00:06:02 +0100

    Added "Added to cart" verification line above button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,23 +7,19 @@ products.forEach((product) => {
         <img class="product-image"
           src="${product.image}">
       </div>
-
       <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
-
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars*10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
       </div>
-
       <div class="product-price">
         $${(product.priceCents/100).toFixed(2)}
       </div>
-
       <div class="product-quantity-container">
         <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
@@ -38,10 +34,8 @@ products.forEach((product) => {
           <option value="10">10</option>
         </select>
       </div>
-
       <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -50,9 +44,7 @@ products.forEach((product) => {
         Add to Cart
       </button>
     </div>`;
-})
-
-console.log(productsHTML);
+});
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
@@ -62,17 +54,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     let matchingItem;
 
     cart.forEach((item) => {
-      if(productId === item.productId) {
-        matchingItem = item;
-      }
+      if(productId === item.productId)matchingItem = item;
     });
 
     const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
     const quantity = Number(quantitySelector.value);
 
-    if(matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
+    if(matchingItem) matchingItem.quantity += quantity;
+    else {
       cart.push({
         productId,
         quantity
@@ -81,10 +70,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
     let cartQuantity = 0;
 
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    })
+    cart.forEach((item) => {cartQuantity += item.quantity;})
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+    
+    addedMessage.classList.add('added-to-cart-visible');
   });
 });
