606f689ba8876d203a46c8ea08bce6706b7a2fe4
Sergei Fladung <sergeifladung@web.de>
2025-02-23 00:50:09 +0100

    Eddited timer-function on Added-to-cart verification to renew on multiple use

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -7,9 +7,11 @@ products.forEach((product) => {
         <img class="product-image"
           src="${product.image}">
       </div>
+
       <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
+
       <div class="product-rating-container">
         <img class="product-rating-stars"
           src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -17,9 +19,11 @@ products.forEach((product) => {
           ${product.rating.count}
         </div>
       </div>
+
       <div class="product-price">
-        $${(product.priceCents/100).toFixed(2)}
+        $${(product.priceCents / 100).toFixed(2)}
       </div>
+
       <div class="product-quantity-container">
         <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
@@ -34,48 +38,92 @@ products.forEach((product) => {
           <option value="10">10</option>
         </select>
       </div>
+
       <div class="product-spacer"></div>
+
       <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
       </button>
-    </div>`;
+    </div>
+  `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () =>  {
-    const {productId} = button.dataset;
-    let matchingItem;
+// We're going to use an object to save the timeout ids.
+// The reason we use an object is because each product
+// will have its own timeoutId. So an object lets us
+// save multiple timeout ids for different products.
+// For example:
+// {
+//   'product-id1': 2,
+//   'product-id2': 5,
+//   ...
+// }
+// (2 and 5 are ids that are returned when we call setTimeout).
+const addedMessageTimeouts = {};
 
-    cart.forEach((item) => {
-      if(productId === item.productId)matchingItem = item;
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const {productId} = button.dataset;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const quantity = Number(quantitySelector.value);
+
+      if (matchingItem) {
+        matchingItem.quantity += quantity;
+      } else {
+        cart.push({
+          productId,
+          quantity
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add('added-to-cart-visible');
+
+      // Check if there's a previous timeout for this
+      // product. If there is, we should stop it.
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      // Save the timeoutId for this product
+      // so we can stop it later if we need to.
+      addedMessageTimeouts[productId] = timeoutId;
     });
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-    const quantity = Number(quantitySelector.value);
-
-    if(matchingItem) matchingItem.quantity += quantity;
-    else {
-      cart.push({
-        productId,
-        quantity
-      })
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {cartQuantity += item.quantity;})
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-    
-    addedMessage.classList.add('added-to-cart-visible');
   });
-});
