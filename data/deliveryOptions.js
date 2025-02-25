298d0c6e7f92f943532872ff3a49cb630d65795a
Sergei Fladung <sergeifladung@web.de>
2025-02-26 00:26:42 +0100

    Create automated tests for formatCurrency

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -27,7 +27,23 @@ export function getDeliveryOptions(deliveryOptionId) {
 }
 
 export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-  return deliveryDate.format('dddd, MMMM D');
+  let deliveryDate = dayjs();
+  let remainingDays = deliveryOption.deliveryDays;
+  console.log(remainingDays)
+  
+  while (remainingDays>0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+    if(!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
+  
+  const dateString = deliveryDate.format('dddd, MMMM D');
+
+  return dateString;
+}
+
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
 }
