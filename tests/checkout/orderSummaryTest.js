c19fa1c44b8cc83bb55ce70753ec515034e84dcf
Sergei Fladung <sergeifladung@web.de>
2025-03-09 20:31:32 +0100

    Exercises 16 a-h

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,11 +1,14 @@
-import {renderOrderSummary} from "../../scripts/checkout/orderSummary.js";
-import {renderPaymentSummary} from "../../scripts/checkout/paymentSummary.js";
-import {loadFromStorage, cart} from '../../data/cart.js'
+import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
+import {loadFromStorage, cart} from '../../data/cart.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-  const productId2 = '83d4ca15-0f35-48f5-b7a3-1ea210004f2e';
-  
+  const productName1 = 'Black and Gray Athletic Cotton Socks - 6 Pairs'
+  const productPrice1 = '$10.90';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+  const productName2 = 'Intermediate Size Basketball';
+  const productPrice2 = '$20.95';
+
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
 
@@ -15,21 +18,21 @@ describe('test suite: renderOrderSummary', () => {
     `;
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
-        return JSON.stringify([{
-          productId: productId1,
-          quantity: 2,
-          deliveryOptionId: '1'
-        }, {
-          productId: productId2,
-          quantity: 1,
-          deliveryOptionId: '2'
-        }]);
-      });
-      loadFromStorage();
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      }, {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: '2'
+      }]);
+    });
+    loadFromStorage();
 
-      renderOrderSummary();
+    renderOrderSummary();
   });
-  
+
   afterEach(() => {
     document.querySelector('.js-test-container').innerHTML = '';
   });
@@ -39,25 +42,40 @@ describe('test suite: renderOrderSummary', () => {
       document.querySelectorAll('.js-cart-item-container').length
     ).toEqual(2);
     expect(
-      document.querySelector(`.js-product-quantity-${productId1}`)
-    .innerText).toContain('Quantity: 2');
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText
+    ).toContain('Quantity: 2');
     expect(
-      document.querySelector(`.js-product-quantity-${productId2}`)
-    .innerText).toContain('Quantity: 1');
-  })
-  
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText
+    ).toContain('Quantity: 1');
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual(productName1);
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual(productPrice1);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual(productName2);
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual(productPrice2);
+  });
+
   it('removes a product', () => {
-      document.querySelector(`.js-delete-link-${productId1}`).click();
-      expect(
-        document.querySelectorAll('.js-cart-item-container').length
-      ).toEqual(1);
-      expect(
-        document.querySelector(`.js-cart-item-container-${productId1}`)
-      ).toEqual(null);
-      expect(
-        document.querySelector(`.js-cart-item-container-${productId2}`)
-      ).not.toEqual(null);
-      expect(cart.length).toEqual(1);
-      expect(cart[0].productId).toEqual(productId2);
-  })
+    document.querySelector(`.js-delete-link-${productId1}`).click();
+    expect(
+      document.querySelectorAll('.js-cart-item-container').length
+    ).toEqual(1);
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId1}`)
+    ).toEqual(null);
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`)
+    ).not.toEqual(null);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual(productName2);
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+  });
 });
