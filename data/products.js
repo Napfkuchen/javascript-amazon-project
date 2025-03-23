aeaf488cdef92d31d41214b62af778a8e9623fb9
Sergei Fladung <sergeifladung@web.de>
2025-03-23 23:39:39 +0100

    Exercises 17 a-i

--- data/products.js
+++ data/products.js
@@ -12,7 +12,7 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
-class Product {
+export class Product {
   id;
   image;
   name;
@@ -40,7 +40,7 @@ class Product {
   }
 }
 
-class Clothing extends Product {
+export class Clothing extends Product {
   sizeChartLink;
 
   constructor(productDetails) {
@@ -55,6 +55,28 @@ class Clothing extends Product {
       </a>
     `;
   };
+}
+
+export class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.instructionsLink}" target="_blank">
+        Instructions
+      </a>
+      <a href="${this.warrantyLink}" target="_blank">
+        Warranty
+      </a>
+    `;
+  }
 }
 
 /* 
@@ -148,7 +170,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -778,6 +803,9 @@ export const products = [
 ].map((productDetails) => {
   if(productDetails.type === 'clothing') {
     return new Clothing(productDetails);
+  } else if(productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   }
+
   return new Product(productDetails);
 });
